ed9c42440e53d2b2421931489d920a6da70e0715
abdulF15 <aafattahh@gmail.com>
2023-03-03 10:01:23 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+//toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika hamburgerMenu di click
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// klik diluar sidebar untuk menghilangkan nav
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
